6dde0552a429b9df8377a11dd570ca6ed533c509
Codeeq <errytage@gmail.com>
2026-03-02 17:38:12 +0100

    add inactivr sidebar icons

--- components/sidebar/sidebarData.tsx
+++ components/sidebar/sidebarData.tsx
@@ -1,6 +1,13 @@
 import {
   CalendarIcon,
   DashboardIcon,
+  LineCalendarIcon,
+  LineDashboardIcon,
+  LineMenteeIcon,
+  LineMentorIcon,
+  LineSettingsIcon,
+  LineTemplateIcon,
+  LineTicketStarIcon,
   MenteeIcon,
   MentorIcon,
   SettingsIcon,
@@ -11,57 +18,68 @@ import {
 export const SidebarData = [
   {
     icon: <DashboardIcon />,
+    lineIcon: <LineDashboardIcon />,
     title: "Dashboard",
     url: "/dashboard",
   },
   {
     icon: <TemplateIcon />,
+    lineIcon: <LineTemplateIcon />,
     title: "Verifications",
     url: "/verifications",
   },
   {
     icon: <CalendarIcon />,
+    lineIcon: <LineCalendarIcon />,
     title: "Appointments",
     url: "/appointments",
   },
   {
     icon: <MentorIcon />,
+    lineIcon: <LineMentorIcon />,
     title: "Mentors",
     url: "/mentors",
   },
   {
     icon: <MenteeIcon />,
+    lineIcon: <LineMenteeIcon />,
     title: "Mentees",
     url: "/mentees",
   },
   {
     icon: <TicketStarIcon />,
+    lineIcon: <LineTicketStarIcon />,
     title: "Transactions",
     url: "/transactions",
   },
   {
     icon: <TicketStarIcon />,
+    lineIcon: <LineTicketStarIcon />,
     title: "Payouts",
     url: "/payouts",
   },
 
   {
     icon: <SettingsIcon />,
+    lineIcon: <LineSettingsIcon />,
     title: "Settings",
     url: "/settings",
     childRoutes: [
       {
         icon: null,
+        lineIcon: null,
         title: "Basic",
         url: "/settings/basic",
       },
       {
         icon: null,
+        lineIcon: null,
         title: "Security",
         url: "/settings/security",
       },
       {
         icon: null,
+        lineIcon: null,
         title: "Administrator",
         url: "/settings/administrator",
       },
