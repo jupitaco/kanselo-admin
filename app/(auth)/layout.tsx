3205fc119af67672c93f5908e5a0613ef4837641
Codeeq <errytage@gmail.com>
2026-03-10 14:33:17 +0100

    ui changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,17 +7,16 @@ export default function AuthLayout({
 }>) {
   return (
     <main className="">
-      <main className="no-scrollbar mx-auto flex h-screen w-full max-w-xl  flex-col items-center justify-between gap-8 overflow-y-auto py-7">
+      <main className="no-scrollbar mx-auto flex h-screen w-full max-w-xl flex-col items-center justify-between gap-8 overflow-y-auto py-7">
         <BrandLogo />
         <section className="w-full px-2">{children}</section>
 
         <footer>
           <small className="text-grey-300">
-            @2025 Kanselo All Right Reserved.
+            @{new Date().getFullYear()} Kanselo All Right Reserved.
           </small>
         </footer>
       </main>
-
     </main>
   );
 }
